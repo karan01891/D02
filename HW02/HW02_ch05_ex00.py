69d6d71ad4199bbb03b25433b87a9045fcd3195b
Karan Rao <karanrao@karans-air.attlocal.net>
2016-08-03 18:30:21 -0700

    answers

--- HW02/HW02_ch05_ex00.py
+++ HW02/HW02_ch05_ex00.py
@@ -8,11 +8,12 @@
 # Write your functions below:
 # Body
 
-
-
-
-
-
+def do_n(print_hello,n):
+    if (n <= 0):
+     quit()
+    
+    print_hello()
+    do_n (print_hello, n-1)
 
 
 
@@ -21,13 +22,8 @@ def print_hello():
     print("Hello World")
 ################################################################################
 def main():
-    """Call your function within this function.
-    When complete have one function call in this function:
-    do_n(print_hello, 10)
-    """
-    print_hello() # replace this with do_n(print_hello, 10)
-
-
-
+    
+    do_n(print_hello,10)
+    
 if __name__ == "__main__":
     main()
